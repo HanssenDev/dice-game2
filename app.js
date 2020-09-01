bcb45568fc19f1a17160719668876392a57ef7d7
Hanssen <hramanjooloo@gmail.com>
2020-09-01 18:59:30 +0100

    add logic for dice roll

--- app.js
+++ app.js
@@ -8,42 +8,66 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 To-do
-1. Dice roll
+1. Dice roll - DONE
 2. Player turn
 3. Win condition
 4. Hold
+5. If player rolls 1
 
 */
 
-// Declare constants for buttons
+// Declare constants for buttons and dies
 const newGameBtn = document.querySelector(".btn-new");
 const rollBtn = document.querySelector(".btn-roll");
 const holdBtn = document.querySelector(".btn-hold");
+const dice = document.querySelectorAll(".dice");
+
+document.querySelector(".dice");
 
 // Declare constants for player scores
-const p1Total = document.getElementById("score-0").textContent;
-const p1Current = document.getElementById("current-0").textContent;
-const p2Total = document.getElementById("score-1").textContent;
-const p2Current = document.getElementById("current-1").textContent;
+const player1 = document.querySelector(".player-0-panel").children;
+const player2 = document.querySelector(".player-1-panel").children;
+
+let p1Total = player1[1];
+let p1Current = player1[2].children[1].textContent;
+let p2Total = player2[1];
+let p2Current = player2[2].children[1].textContent;
+
+let gamePlaying = false;
+
+const players = ["Player 1", "Player 2"];
+let activePlayer = players[0];
+
+// Declare functions
+const startGame = () => {
+  gamePlaying = true;
+  p1Total.textContent = 0;
+  p2Total.textContent = 0;
+
+  dice.forEach((die) => {
+    die.style.display = "none";
+  });
+};
+
+const rollDice = () => {
+  if (gamePlaying) {
+      
+    // Make random dice image appear
+    dice.forEach((die) => {
+      die.style.display = "block";
+      die.src = `/img/dice-${Math.floor(Math.random() * 6) + 1}.png`;
+    });
+  }
+};
+
+const holdScore = () => {
+ p1Total.textContent >= 100 ? player1[0].textContent = "WINNER!" : null
+ p2Total.textContent >= 100 ? player2[0].textContent = "WINNER!" : null
+}
+
+const roll1 = () => {};
 
 // Event listeners
 newGameBtn.addEventListener("click", startGame);
 rollBtn.addEventListener("click", rollDice);
 holdBtn.addEventListener("click", holdScore);
-
-
-// Declare functions
-const startGame = () => {
-    p1Total = 0;
-    p2Total = 0;
-}
-
-const rollDice = () => {
-
-}
-
-
-const holdScore = () => {
-
-}
-
